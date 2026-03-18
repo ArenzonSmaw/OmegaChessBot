7cd3dc4d76d842baa386d221de4f389734455483
Arenzon <arenzonlior@gmail.com>
2026-03-18 23:45:22 +0200

    lexer 1.0

--- CompileProj/lexer.c
+++ CompileProj/lexer.c
@@ -6,44 +6,53 @@
 
 #pragma warning(disable:4996)
 
+#define NUM_OF_STATES 106
+#define NUM_OF_INPUTS 32
+#define NUM_OF_CHARS 128
+
 
 token_list node() {
 	return (token_list)malloc(sizeof(token_node));
 }
 
-void unknown_character(token_list lst, char value);
-void illegal_character(token_list lst, char value);
+void unknown_character(token_list *lst, char value);
+void illegal_character(token_list *lst, char value);
 
-void start_token(token_list lst, char value);
+void start_token(token_list *lst, char value);
 
-void start_natural(token_list lst, char value);
-void start_rational(token_list lst, char value);
-void start_int(token_list lst, char value);
-void start_float(token_list lst, char value);
-void start_bool(token_list lst, char value);
-void start_char(token_list lst, char value);
-void start_string(token_list lst, char value);
-void start_ident(token_list lst, char value);
+void start_natural(token_list *lst, char value);
+void start_rational(token_list* lst, char value) {}
+void start_int(token_list* lst, char value) {}
+void start_float(token_list* lst, char value) {}
+void start_bool(token_list *lst, char value);
+void start_char(token_list *lst, char value);
+void start_string(token_list *lst, char value);
+void start_ident(token_list *lst, char value);
 
-void start_controlflow(token_list lst, char value);
-void add_controlflow(token_list lst, char value);
+void start_controlflow(token_list* lst, char value);
+void add_controlflow(token_list *lst, char value);
 
-void add_char(token_list lst, char value);
-void stay(token_list lst, char value) {}
-void skip(token_list lst, char value) {}
+void add_char(token_list *lst, char value);
+void add_digit(token_list *lst, char value);
+void stay(token_list* lst, char value);
+void skip(token_list *lst, char value) {}
 
-void start_operator(token_list lst, char value);
-void add_operator(token_list lst, char value);
-void end_operator(token_list lst, char value) {}
+void start_operator(token_list *lst, char value);
+void add_operator(token_list *lst, char value);
+void end_operator(token_list *lst, char value) {}
 
-void add_token(token_list lst, char value) {}
-void add_current(token_list lst, char value);
+void start_vartype(token_list* lst, char value);
+void start_errorhandler(token_list* lst, char value);
+void start_declare(token_list* lst, char value);
+void start_conditional(token_list* lst, char value);
+
+void add_token(token_list *lst, char value) {}
+void add_current(token_list* lst, char value) {}
 
 
-
-int CHAR_CLASS[128] = { PRINTABLE };
-int GOTO[106][30] = { 105 };
-void (*ACTION[106][30])(token_list, char) = { illegal_character };
+static int CHAR_CLASS[NUM_OF_CHARS] = { PRINTABLE };
+static int GOTO[NUM_OF_STATES][NUM_OF_INPUTS] = { 105 };
+static void (*ACTION[NUM_OF_STATES][NUM_OF_INPUTS])(token_list*, char) = { illegal_character };
 
 void init_tables()
 {
@@ -94,34 +103,39 @@ void init_tables()
 		CHAR_CLASS[ch] = DIGIT;
 
 	//general operators
-	CHAR_CLASS['!'] = CHAR_CLASS['%'] = CHAR_CLASS['&'] = CHAR_CLASS[':'] = CHAR_CLASS['*'] = CHAR_CLASS['+'] = CHAR_CLASS['|'] = CHAR_CLASS['\\'] = OPERATOR;
+	CHAR_CLASS['!'] = CHAR_CLASS['%'] = CHAR_CLASS['&'] = CHAR_CLASS[':'] = CHAR_CLASS['*'] = CHAR_CLASS['+'] = CHAR_CLASS['|'] = CHAR_CLASS['\\'] = OP;
 	for (ch = '<'; ch <= '>'; ch++)
-		CHAR_CLASS[ch] = OPERATOR;
-	//operators that can affect tokens
+		CHAR_CLASS[ch] = OP;
+	//operators that can affect token types
 	CHAR_CLASS['-'] = MINUS;
 	CHAR_CLASS['.'] = DOT;
 	CHAR_CLASS['/'] = DIVIDE;
 
 	//control flow punctuation
-	CHAR_CLASS['"'] = CHAR_CLASS[','] = CHAR_CLASS['['] = CHAR_CLASS[']'] = CHAR_CLASS['{'] = CHAR_CLASS['}'] = CONTROLFLOW;
+	CHAR_CLASS[','] = CHAR_CLASS['['] = CHAR_CLASS[']'] = CHAR_CLASS['{'] = CHAR_CLASS['}'] = CHAR_CLASS[';'] = CF;
 	for (ch = '\''; ch <= ')'; ch++)
-		CHAR_CLASS[ch] = CONTROLFLOW;
+		CHAR_CLASS[ch] = CF;
+
+	//quotation marks
+	CHAR_CLASS['\''] = SQUOTE;
+	CHAR_CLASS['\"'] = DQUOTE;
 
 	//GOTO + ACTION TBL
 	for (st = 0; st < 106; st++)
 	{
 		GOTO[st][WHITESPACE] = 0;
-		ACTION[st][WHITESPACE] = add_token;
+		ACTION[st][WHITESPACE] = skip;
 
-		GOTO[st][CONTROLFLOW] = 0;
-		ACTION[st][CONTROLFLOW] = add_controlflow;
+		GOTO[st][CF] = 0;
+		ACTION[st][CF] = add_controlflow;
 
-		for (ch = OPERATOR; ch < DOT; ch++)
-		{
-			GOTO[st][ch] = 4;
-			ACTION[st][ch] = add_operator;
-		}
+		GOTO[st][OP] = 4;
+		ACTION[st][OP] = add_operator;
+		
 	}
+	//floating point
+	/*GOTO[0][DOT] = 2; for future development
+	ACTION[0][DOT] = start_float;*/
 
 	//default state
 	GOTO[0][WHITESPACE] = 0;
@@ -152,6 +166,8 @@ void init_tables()
 		GOTO[103][ch] = 103;
 		ACTION[103][ch] = add_char;
 	}
+	GOTO[103][WHITESPACE] = 103;
+	ACTION[103][WHITESPACE] = add_char;
 	GOTO[103][PRINTABLE] = 103;
 	ACTION[103][PRINTABLE] = add_char;
 
@@ -180,42 +196,44 @@ void init_tables()
 	//numeral state
 	GOTO[1][DIGIT] = 1;
 	ACTION[1][DIGIT] = add_digit;
-	GOTO[1][DOT] = 2;
-	GOTO[1][MINUS] = 4;
-	GOTO[1][DIVIDE] = 3;
-	for (ch = DIVIDE; ch <= DOT; ch++)
-	{
-		ACTION[1][ch] = add_operator;
-	}
+	
+	/*GOTO[1][DOT] = 2; for future development
+	ACTION[1][DOT] = to_float;
 
-	//'dot' token
-	GOTO[2][DIGIT] = 1;
-	ACTION[2][DIGIT] = add_char;
-	GOTO[2][OPERATOR] = 4;
-	ACTION[2][OPERATOR] = start_operator;
+	GOTO[1][DIVIDE] = 2;
+	ACTION[1][DIVIDE] = to_rational;*/
 
-	//'division' token
-	GOTO[3][DIGIT] = 1;
-	ACTION[3][DIGIT] = start_natural;
-	GOTO[3][WHITESPACE] = 105;
 
 	//operators state
-	GOTO[4][WHITESPACE] = 4;
+	GOTO[4][WHITESPACE] = 0;
 	ACTION[4][WHITESPACE] = skip;
-	for (ch = OPERATOR; ch <= DOT; ch++) 
-	{
-		GOTO[4][ch] = 4;
-		ACTION[4][ch] = add_operator;
-	}
+	GOTO[4][OPERATOR] = 4;
+	ACTION[4][OPERATOR] = add_operator;
 	GOTO[4][DIGIT] = 1;
 	ACTION[4][DIGIT] = start_natural;
 	GOTO[4][SQUOTE] = 104;
 	ACTION[4][SQUOTE] = start_char;
 	GOTO[4][DQUOTE] = 103;
 	ACTION[4][DQUOTE] = start_string;
-
 	for (ch = LETTER; ch <= X; ch++)
+	{
 		GOTO[4][ch] = 5;
+		ACTION[4][ch] = start_ident;
+	}
+	GOTO[4][B] = 6;
+	GOTO[4][C] = 14;
+	GOTO[4][D] = 21;
+	GOTO[4][E] = 28;
+	GOTO[4][F] = 40;
+	GOTO[4][I] = 45;
+	GOTO[4][L] = 49;
+	GOTO[4][N] = 55;
+	GOTO[4][P] = 62;
+	GOTO[4][R] = 72;
+	GOTO[4][S] = 85;
+	GOTO[4][U] = 95;
+	GOTO[4][V] = 98;
+
 
 	//identifiers
 	for (ch = DIGIT; ch <= X; ch++)
@@ -234,27 +252,34 @@ void init_tables()
 			ACTION[st][ch] = add_char;
 		}
 		GOTO[st][PRINTABLE] = 5;
-		GOTO[st][PRINTABLE] = add_char;
+		ACTION[st][PRINTABLE] = add_char;
+
+		GOTO[st][WHITESPACE] = 0;
+		ACTION[st][WHITESPACE] = skip;
 	}
 
 	// keywords
 	GOTO[6][O] = 7; //b|ool
 	GOTO[7][O] = 8; //bo|ol
 	GOTO[8][L] = 9; //boo|l
+	ACTION[8][L] = start_vartype;
 
 	GOTO[6][R] = 10; //b|reak
 	GOTO[10][E] = 11; //br|eak
 	GOTO[11][A] = 12; //bre|ak
 	GOTO[12][K] = 13; //brea|k
+	ACTION[12][K] = start_controlflow;
 
 	GOTO[14][H] = 15; //c|har , c|heck
-
-	GOTO[19][R] = 20; //cha|r
+	
 	GOTO[15][A] = 19; //ch|ar
+	GOTO[19][R] = 20; //cha|r
+	ACTION[19][R] = start_vartype;
 
 	GOTO[15][E] = 16; //ch|eck
 	GOTO[16][C] = 17; //che|ck
 	GOTO[17][K] = 18; //chec|k
+	ACTION[17][K] = start_errorhandler;
 
 	GOTO[21][E] = 22; //d|ecalre
 	GOTO[22][C] = 23; //de|clare
@@ -262,12 +287,14 @@ void init_tables()
 	GOTO[24][A] = 25; //decl|are
 	GOTO[25][R] = 26; //decla|re
 	GOTO[26][E] = 27; //declar|e
+	ACTION[26][E] = start_declare;
 
 	GOTO[28][L] = 29; //e|lse
 	GOTO[28][X] = 32; //e|xception
 
 	GOTO[29][S] = 30; //el|se
 	GOTO[30][E] = 31; //els|e
+	ACTION[30][E] = start_conditional;
 
 	GOTO[32][C] = 33; //ex|ception
 	GOTO[33][E] = 34; //exc|eption
@@ -276,25 +303,30 @@ void init_tables()
 	GOTO[36][I] = 37; //except|ion
 	GOTO[37][O] = 38; //excepti|on
 	GOTO[38][N] = 39; //exceptio|n
+	ACTION[38][N] = start_errorhandler;
 
 	GOTO[40][L] = 41; //f|loat
 	GOTO[41][O] = 42; //fl|oat
 	GOTO[42][A] = 43; //flo|at
 	GOTO[43][T] = 44; //floa|t
+	ACTION[43][T] = start_vartype;
 
 	GOTO[45][F] = 46; //i|f
+	ACTION[45][F] = start_conditional;
 
 	GOTO[45][N] = 47; //i|nt
 	GOTO[47][T] = 48; //in|t
+	ACTION[47][T] = start_vartype;
 
 	GOTO[49][O] = 50; //l|ong, l|oop
 	
 	GOTO[50][O] = 53; //lo|op
 	GOTO[53][P] = 54; //loo|p
+	ACTION[53][P] = start_controlflow;
 
-	GOTO[51][G] = 52; //lon|g
 	GOTO[50][N] = 51; //lo|ng
-	
+	GOTO[51][G] = 52; //lon|g
+	ACTION[51][G] = start_vartype;
 
 	GOTO[55][A] = 56; //n|atural
 	GOTO[56][T] = 57; //na|tural
@@ -302,10 +334,12 @@ void init_tables()
 	GOTO[58][R] = 59; //natu|ral
 	GOTO[59][A] = 60; //natur|al
 	GOTO[60][L] = 61; //natura|l
+	ACTION[60][L] = start_vartype;
 
 	GOTO[62][A] = 63; //p|ass
 	GOTO[63][S] = 64; //pa|ss
 	GOTO[64][S] = 65; //pas|s
+	ACTION[64][S] = start_controlflow;
 
 	GOTO[62][O] = 66; //p|ointer
 	GOTO[66][I] = 67; //po|inter
@@ -313,6 +347,7 @@ void init_tables()
 	GOTO[68][T] = 69; //poin|ter
 	GOTO[69][E] = 70; //point|er
 	GOTO[70][R] = 71; //pointe|r
+	ACTION[70][R] = start_vartype;
 
 	GOTO[72][A] = 73; //r|ational
 	GOTO[73][T] = 74; //ra|tional
@@ -321,30 +356,36 @@ void init_tables()
 	GOTO[76][N] = 77; //ratio|nal
 	GOTO[77][A] = 78; //ration|al
 	GOTO[78][L] = 79; //rationa|l
+	ACTION[78][L] = start_vartype;
 
 	GOTO[72][E] = 80; //r|eturn
 	GOTO[80][T] = 81; //re|turn
 	GOTO[81][U] = 82; //ret|urn
 	GOTO[82][R] = 83; //retu|rn
 	GOTO[83][N] = 84; //retur|n
+	ACTION[83][N] = start_controlflow;
 
 	GOTO[85][H] = 86; //s|hort
 	GOTO[86][O] = 87; //sh|ort
 	GOTO[87][R] = 88; //sho|rt
 	GOTO[88][T] = 89; //shor|t
+	ACTION[88][T] = start_vartype;
 
 	GOTO[85][T] = 90; //s|tring
 	GOTO[90][R] = 91; //st|ring
 	GOTO[91][I] = 92; //str|ing
 	GOTO[92][N] = 93; //stri|ng
 	GOTO[93][G] = 94; //strin|g
+	ACTION[93][G] = start_vartype;
 
 	GOTO[95][S] = 96; //u|se
 	GOTO[96][E] = 97; //us|e
+	ACTION[96][E] = start_declare;
 
 	GOTO[98][O] = 99;  //v|oid
 	GOTO[99][I] = 100; //vo|id
 	GOTO[100][D] = 101;//voi|d
+	ACTION[100][D] = start_vartype;
 
 	//error handling - panic mode recovery
 	for (ch = UNKNOWN; ch <= PRINTABLE; ch++)
@@ -359,80 +400,132 @@ void init_tables()
 	}
 }
 
-void start_token(token_list lst, char ch)
+void start_token(token_list *lst, char ch)
 {
 	token_list token = node();
 	strcpy(token->info.str_val, "\0");
-	token->next = lst->next;
-	lst->next = token;
-	lst = token;
+	if (*lst != NULL)
+	{
+		token->next = (*lst)->next;
+		
+		(*lst)->next = token;
+		*lst = token;
+	}
+	else 
+	{
+		token->next = lst;
+		*lst = token;
+	}
 }
-void start_natural(token_list lst, char value)
+void start_natural(token_list *lst, char value)
 {
 	token_list token = node();
 	token->info.num_val = value - '0';
 	token->type = NATURAL;
-	token->next = lst->next;
-	lst->next = token;
-	lst = token;
+	if (*lst != NULL)
+	{
+		token->next = (*lst)->next;
+		(*lst)->next = token;
+		*lst = token;
+	}
+	else
+	{
+		token->next = *lst;
+		*lst = token;
+	}
 }
-void add_digit(token_list lst, char value)
+//void start_int(token_list* lst, char value)
+//{
+//	start_natural(lst, value);
+//	(*lst)->type = INT;
+//}
+void add_digit(token_list *lst, char value)
 {
-	lst->info.num_val *= 10;
-	lst->info.num_val += value - '0';
+	(*lst)->info.num_val *= 10;
+	(*lst)->info.num_val += value - '0';
 }
-void start_bool(token_list lst, char value)
+void start_bool(token_list *lst, char value)
 {
 	add_char(lst, value);
-	lst->type = BOOL;
+	(*lst)->type = BOOL;
 }
-void start_char(token_list lst, char value)
+void start_char(token_list *lst, char value)
 {
 	start_token(lst, value);
 	add_char(lst, value);
-	lst->type = CHAR;
+	(*lst)->type = CHAR;
 }
-void start_string(token_list lst, char value)
+void start_string(token_list *lst, char value)
 {
 	start_token(lst, value);
 	add_char(lst, value);
-	lst->type = STRING;
+	(*lst)->type = STRING;
 }
-void start_ident(token_list lst, char value)
+void start_ident(token_list *lst, char value)
 {
 	start_token(lst, value);
 	add_char(lst, value);
-	lst->type = IDENTIFIER;
+	(*lst)->type = IDENTIFIER;
 }
 
-void add_controlflow(token_list lst, char value)
+void add_controlflow(token_list *lst, char value)
 {
 	start_token(lst, value);
 	add_char(lst, value);
-	lst->type = CONTROLFLOW;
+	(*lst)->type = CONTROLFLOW;
 }
-
-void add_char(token_list lst, char value)
+void start_controlflow(token_list* lst, char value)
 {
-	strcat(lst->info.str_val, value + '\0');
+	add_controlflow(lst, value);
 }
 
-void start_operator(token_list lst, char value)
+void add_char(token_list *lst, char value)
+{
+	strcat((*lst)->info.str_val, (char[2]){value, '\0'});
+}
+
+void start_operator(token_list *lst, char value)
 {
 	start_token(lst, value);
 	add_char(lst, value);
-	lst->type = OPERATOR;
+	(*lst)->type = OPERATOR;
 }
-void add_operator(token_list lst, char value)
+void add_operator(token_list *lst, char value)
 {
 	start_operator(lst, value);
 }
 
-void unknown_character(token_list lst, char value)
+void start_vartype(token_list* lst, char value)
+{
+	add_char(lst, value);
+	(*lst)->type = VARTYPE;
+}
+void start_errorhandler(token_list* lst, char value)
+{
+	add_char(lst, value);
+	(*lst)->type = ERROR_HANDLER;
+}
+void start_declare(token_list* lst, char value)
+{
+	add_char(lst, value);
+	(*lst)->type = DECLARE;
+}
+void start_conditional(token_list* lst, char value)
+{
+	add_char(lst, value);
+	(*lst)->type = CONDITIONAL;
+}
+
+void stay(token_list* lst, char value)
+{
+	add_char(lst, value);
+}
+
+void unknown_character(token_list *lst, char value)
 {
 	printf("unknown character %c", value);
 }
-void illegal_character(token_list lst, char value)
+void illegal_character(token_list *lst, char value)
 {
 	printf("illegal character %c", value);
 }
@@ -442,8 +535,15 @@ token_list tokenize(char text[])
 	char ch = *text;
 	INPUT ch_class;
 	int state = 0;
-	token_list lst = (token_list)malloc(sizeof(token_node));
-	void (*action)(token_list, char);
+	token_list list = node();
+	token_list pos;
+	void (*action)(token_list*, char);
+
+	init_tables(CHAR_CLASS, GOTO, ACTION);
+
+	list->next = NULL;
+	list->type = HEADER;
+	pos = list;
 
 	while (ch != '\0')
 	{
@@ -452,8 +552,10 @@ token_list tokenize(char text[])
 		action = ACTION[state][ch_class];
 		state = GOTO[state][ch_class];
 
-		action(lst, ch);
+		action(&pos, ch);
+
+		ch = *(++text);
 	}
 
-	return lst;
+	return list;
 }
--- CompileProj/main.c
+++ CompileProj/main.c
@@ -6,26 +6,32 @@
 
 int main() {
 
-	token_list lst = tokenize("int mispar = 5;");
-	
+	token_list lst;
+	lst = tokenize("declare mispar: int = 57;\n declare str:string = \"blah blah\";");
+	//lst = tokenize("\n \'t\' return");
+	token_list temp;
+	char types[15][20] = { "HEADER", "INT", "CONTROLFLOW", "CHAR", "FLOAT", "OPERATOR", "STRING", "NATURAL", "RATIONAL", "BOOL", "CONDITIONAL", "IDENTIFIER", "VARTYPE", "ERROR_HANDLER", "DECLARE" };
 	while (lst)
 	{
 		switch (lst->type)
 		{
-		case INT:
-		case CHAR:
-		case FLOAT:
-		case STRING:
-		case RATIONAL:
-		case BOOL:
 		case IDENTIFIER:
-			printf("%s: %s", lst->info.str_val, lst->type);
+		case STRING:
+		case CONTROLFLOW:
+		case OPERATOR:
+		case VARTYPE:
+		case DECLARE:
+			printf("%s: %s", lst->info.str_val, types[lst->type]);
 			break;
 		case NATURAL:
-			printf("%d: %s", lst->info.num_val, lst->type);
+		case BOOL:
+			printf("%d: %s", lst->info.num_val, types[lst->type]);
 			break;
 		}
-		printf('\n');
+		printf("\n");
+		temp = lst;
+		lst = lst->next;
+		free(temp);
 	}
 
 	return 0;
